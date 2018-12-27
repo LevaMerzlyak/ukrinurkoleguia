5b7322a018c04ee85208e147f2731ff0255ac76b
Serhiy Levitskiy <13dir13@gmail.com>
2018-12-27 13:33:24 +0300

    add php en files

---
+++ en/vacancies/vacancy.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width" />
+	<title>ukriniurkoleguia</title>
+	<!-- Preloader -->
+	<link rel="stylesheet" type="text/css" href="../../css/preloader.css">
+	<!-- Fonts -->
+	<link rel="stylesheet" type="text/css" href="../../css/fonts.css">
+	<!-- Reset CSS -->
+	<link rel="stylesheet" type="text/css" href="../../css/reset.css">
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="../../css/publication.css">
+
+</head>
+<body class="loading">
+	<div class="loader__wrap"></div>
+	<div class="loader">
+		<div class="loader__bg"></div>
+	</div>
+	
+	<?php include_once "../php/includ/menu_top_en.php";?>
+	
+	<main class="main" id="bg1">
+		<div class="bg__part bg__part_main">
+			<div class="bg__inner"></div>
+		</div>
+		<div class="bg__part bg__part_main">
+			<div class="bg__inner"></div>
+		</div>
+		<div class="bg__part bg__part_main">
+			<div class="bg__inner"></div>
+		</div>
+		<div class="bg__part bg__part_main">
+			<div class="bg__inner"></div>
+		</div>
+		<img class="bg__img" src="../../images/team.jpg">
+		<div class="mainTxt">
+			<div class="textWrap textWrap_head">
+				<h1 class="head anim__txt">The only way to preserve the Ukrainian state - Euro-Atlantic choice of Ukraine.</h1>
+			</div>				
+		</div>
+	</main>
+	<section class="service">
+		<div class="serviceTxt">
+			<div class="textWrap textWrap_txtSup">
+				<p class="service__txt service__txt_sup">The oldest probate research network on the territory of the former Soviet Union.</p>
+			</div>
+			<div class="textWrap">
+				<p class="service__txt"><span>UKRINIURKOLEGUIA</span> is the only Ukrainian office specialized in probate research and handling foreign estate matters. We carry out probate research in Ukraine, Russia, Belarus, Moldova and Poland.</p>
+			</div>
+			<div class="textWrap">
+				<p class="service__txt">Prompt and effective services are ensured by concerted work of the headquaters in Kyiv and its permanent branches in key research regions of Ukraine, namely:</p>
+			</div>
+			<div class="textWrap">
+				<ul class="serviceTxt__list">
+					<li class="serviceTxt__item">Western Ukraine Bureau – Lviv (aka Lwow aka Lemberg), Halychyna (Galicia)</li>
+					<li class="serviceTxt__item">Transcarpathian Bureau – Uzhhorod (aka Uzhorod aka Ungvar aka Ungwar), Carpathian Ruthenia</li>
+					<li class="serviceTxt__item">Office in Volynska and Rivne Regions – Lutsk (aka Luck), Volyn (Wolyn)</li>
+					<li class="serviceTxt__item">Office in Ternopil Region – Ternopil (aka Tarnopol), Halychyna (Galicia)</li>
+					<li class="serviceTxt__item">Office in Chernivtsi Region – Chernivtsi (aka Cernauti aka Czernowitz), Bukovina</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+		<!-- jQuery -->
+	<script type="text/javascript" src="../../js/jquery-3.3.1.min.js"></script>
+		<!-- Preloader -->
+	<script type="text/javascript" src="../../js/preloaderContacts.js"></script>
+		<!-- Backgrounds Init -->
+	<script type="text/javascript" src="../../js/bgInit.js"></script>
+	<script>
+		b1 = new BgInit('#bg1');
+	</script>
+		<!-- Mobile Menu -->
+	<script type="text/javascript" src="../../js/mobMenu.js"></script>
+	<script>
+		m1 = new MobileMenu('#mobMenu');
+	</script>
+</body>
+</html>
